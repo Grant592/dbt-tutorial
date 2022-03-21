ce2f1ca568f72d65290746f59a8319b89fa3558b
grant.beasley@salecycle.com <grant.beasley@salecycle.com>
2022-03-21 20:19:44 +0000

    added dbt tutorial files and dbt_util files

---
+++ models/staging/stripe/stg_payments.sql
@@ -0,0 +1,18 @@
+with payments as (
+
+    select
+        id as payment_id,
+        orderid as order_id,
+        paymentmethod as payment_method,
+        status,
+        {{- cents_to_dollars('amount') }} as amount,
+        created as created_at,
+       
+    from
+        {{ source('stripe', 'payment') }}
+
+)
+
+select * from payments
+
+{{ limit_data_in_dev() }}
